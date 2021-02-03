47551f58f226ca24b0d42d32ca436f0bc0724ce9
Horoshiy <info@dinamchiki.ru>
2021-02-03 14:10:20 +0300

    fix remote browser url

--- src/test/java/tests/ui/TestBase.java
+++ src/test/java/tests/ui/TestBase.java
@@ -1,12 +1,14 @@
 package tests.ui;
 
 import com.codeborne.selenide.Configuration;
+import drivers.CustomWebDriver;
 import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentsHelper.*;
 
@@ -14,13 +16,7 @@ public class TestBase {
     @BeforeAll
     static void setup() {
         addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-
-        Configuration.browserCapabilities = capabilities;
-        Configuration.remote = System.getProperty("webdriver.remote");
+        Configuration.browser = CustomWebDriver.class.getName();
         Configuration.startMaximized = true;
     }
 
@@ -31,5 +27,6 @@ public class TestBase {
         attachPageSource();
         attachAsText("Browser console logs", getConsoleLogs());
         attachVideo();
+        closeWebDriver();
     }
 }
