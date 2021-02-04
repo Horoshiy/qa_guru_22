7c3ee7ada1c30a2f3e5a1ea768cd3bc9b6f4a883
Horoshiy <info@dinamchiki.ru>
2021-02-04 09:53:13 +0300

    add registration form test

--- src/test/java/tests/ui/LoginRegistrationTests.java
+++ src/test/java/tests/ui/LoginRegistrationTests.java
@@ -1,11 +1,13 @@
 package tests.ui;
 
+import io.qameta.allure.AllureId;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -37,6 +39,41 @@ public class LoginRegistrationTests extends TestBase {
             $(byName("auth.passwd")).val(password);
             $("#auth_email a.submit").click();
             $(".logout-user-link").shouldHave(text("выход"));
+        });
+    }
+
+    @Test
+    @AllureId("1542")
+    @DisplayName("Регистрация на сайте")
+    void registration() {
+        final String url = System.getProperty("site.url");
+        final String name = System.getProperty("site.user");
+        final String password = System.getProperty("site.password");
+        final String firstName = "Иван";
+        final String lastName = "Иванов";
+
+        step("Открыть сайт", () -> {
+            open(url);
+            $("#logo").shouldHave(text("спорт"));
+        });
+
+        step("Нажать по ссылке вход/регистрация", () -> {
+            $(".alter_login").click();
+            $(".inner_auth_popup").shouldHave(text("Пожалуйста, авторизуйтесь"));
+        });
+
+        step("Перейти по ссылке Зарегистрироваться", () -> {
+           $("a.change_template").click();
+           $("#auth_email_regustration").shouldBe(visible);
+        });
+
+        step("Заполнить форму", () -> {
+            $(byName("auth.email")).val(name);
+            $(byName("auth.passwd")).val(password);
+            $(byName("auth.first_name")).val(firstName);
+            $(byName("auth.last_name")).val(lastName);
+            $("#auth_email_regustration a.submit").click();
+            $(".inner_auth_popup").shouldHave(text("Вы регистрировались на «Спорт вокруг.» ранее."));
         });
     }
 }
