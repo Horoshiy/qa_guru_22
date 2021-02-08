1c2bfc77100cd997101b1cc5b5f94be1e5108728
Horoshiy <info@dinamchiki.ru>
2021-02-08 15:05:25 +0300

    change display name of checkSportEventLinks

--- src/test/java/tests/ui/SportTrendsHomePageLinksTests.java
+++ src/test/java/tests/ui/SportTrendsHomePageLinksTests.java
@@ -3,6 +3,7 @@ package tests.ui;
 import annotations.JiraIssue;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -22,6 +23,7 @@ public class SportTrendsHomePageLinksTests extends TestBase {
     @JiraIssue("QC3-1")
     @ParameterizedTest
     @ValueSource(strings = {"фигурн", "велоспорт", "легк", "кроссфит", "художественн", "воркаут", "лыжн", "конн", "акробатическ"})
+    @DisplayName("Check links to events from sports")
     void checkSportEventsLinks(String sport) {
         final String url = System.getProperty("site.url");
         parameter("event", sport);
